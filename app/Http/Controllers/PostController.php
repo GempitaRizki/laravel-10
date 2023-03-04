8a973dc73016aa8c544f71181fbe3e50fc7307c2
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-03-04 11:25:27 +0700

    add dashboard

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,10 @@ class PostController extends Controller
     public function index(){
         return view('index');
     }
+
+    public function add(){
+
+        return view('admin.posts.add');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\PostController;
 
 Route::get('/',[PostController::class,'index']);
 
+Route::get('admin/post/add',[PostController::class,'add']);
+
 
 //domainname.com/user/delete
 //coba
